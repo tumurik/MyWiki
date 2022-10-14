c742e6ee7efd2e7d18fee037fb0980ea4503b1ee
Arturs T <arturs.tumans@hotmail.com>
2022-10-14 16:18:14 +0300

    added domain models

---
+++ MyWiki.Web/Models/Domain/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace MyWiki.Web.Models.Domain
+{
+    //all property values must be entered
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string FeaturedFileUrl { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+    }
+}
---
+++ MyWiki.Web/Models/Domain/IssueType.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWiki.Web.Models.Domain
+{
+    public class IssueType
+    {
+        public Guid Id { get; set; } 
+        public string Type { get; set; }
+        public Guid ArticleId { get; set; }
+    }
+}
