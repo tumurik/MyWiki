4a52be9277d875c538013496757b02cf36482526
Arturs T <arturs.tumans@hotmail.com>
2022-10-18 02:20:28 +0300

    Submited article to database

---
+++ MyWiki.Web/Models/ViewModels/CreateArticle.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWiki.Web.Models.ViewModels
+{
+    public class CreateArticle
+    {
+        public string Title { get; set; }
+        public string FullDescription { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
--- MyWiki.Web/Pages/Articles/CreateArticle.cshtml.cs
+++ MyWiki.Web/Pages/Articles/CreateArticle.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyWiki.Web.Data;
+using MyWiki.Web.Models.Domain;
+using MyWiki.Web.Models.ViewModels;
 
 namespace MyWiki.Web.Pages.Articles
 {
     public class CreateArticleModel : PageModel
     {
+        private readonly MyWikiDbContext myWikiDbContext;
+
+        [BindProperty]
+        public CreateArticle CreateArticleRequest { get; set; }
+
+        public CreateArticleModel(MyWikiDbContext myWikiDbContext)
+        {
+            this.myWikiDbContext = myWikiDbContext;
+        }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            var Article = new Article()
+            {
+                Title = CreateArticleRequest.Title,
+                FullDescription = CreateArticleRequest.FullDescription,
+                PublishedDate = CreateArticleRequest.PublishedDate,
+                Author = "Default",
+                Visible = true
+            };
+
+            myWikiDbContext.Articles.Add(Article);
+            myWikiDbContext.SaveChanges();
         }
     }
 }
