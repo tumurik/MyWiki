31d38c18d7f3a8aa9e4225c295aa5b36d3f24a5e
Arturs T <arturs.tumans@hotmail.com>
2022-10-19 03:13:48 +0300

    Show articles in Home page

---
+++ MyWiki.Web/Pages/Articles/ArticleDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWiki.Web.Pages.Articles
+{
+    public class ArticleDetailsModel : PageModel
+    {
+        public void OnGet(string Title)
+        {
+        }
+    }
+}
--- MyWiki.Web/Pages/Index.cshtml.cs
+++ MyWiki.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyWiki.Web.Models.Domain;
+using MyWiki.Web.Repositories;
 
 namespace MyWiki.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IArticleRepository articleRepository;
+        public List<Article> articles { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            IArticleRepository articleRepository)
         {
             _logger = logger;
+            this.articleRepository = articleRepository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            articles = (await articleRepository.GetAllAsync()).ToList();
 
+            return Page();
         }
     }
 }
